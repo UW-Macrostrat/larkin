eb883baf26c785b75af62484269f283cbc6c1937
John J Czaplewski <john@czaplewski.org>
2018-04-15 16:16:22 -0500

    Add runtime validation of each route

--- larkin.js
+++ larkin.js
@@ -66,7 +66,6 @@ module.exports = class Larkin {
       Object.keys(this.routes).forEach(route => {
         routes[this.routes[route].path] = this.routes[route].description
       })
-
       return res.json({
         'v': this.version,
         'license': this.license,
@@ -74,6 +73,7 @@ module.exports = class Larkin {
         'routes': routes
       })
     }
+
     if (!this.routes[requestedRoute]) {
       return this._error(req, res, next, 'Route not found', 404)
     }
@@ -125,6 +125,8 @@ module.exports = class Larkin {
 
   // Given a larkin route definition file register the route on the API
   registerRoute(route) {
+    // Before wiring up validate
+    validateRoute(route)
     // Wire the route up to Express
     this.router.route(route.path)
       .get((req, res, next) => {
@@ -185,5 +187,79 @@ module.exports = class Larkin {
     res.json({
       'error': message
     })
+  }
+}
+
+
+function validateRoute(route) {
+  if (!route) {
+    throw new Error('Route is null')
+  }
+  // Make sure a path is present and properly formatted
+  if (!route.path) {
+    throw new Error('A path is required for the route. For example, /things ')
+  }
+  if (route.path.substring(0, 1) != '/') {
+    throw new Error('The path for a route must begin with a forward slash. For example, /things')
+  }
+
+  // Verify description
+  if (!route.description || !route.description.length) {
+    throw new Error(`The route "${route.path}" is missing a description`)
+  }
+
+  // Make sure parameters are present
+  if (!route.parameters) {
+    throw new Error(`The route "${route.path}" must have parameters`)
+  }
+  if (Object.keys(route.parameters).length === 0 ) {
+    throw new Error(`The route "${route.path}" must have parameters`)
+  }
+
+  // Validate the parameters
+  Object.keys(route.parameters).forEach(param => {
+    if (!route.parameters[param].type) {
+      throw new Error(`The parameter "${param}" is missing a type`)
+    }
+    if (!typeMapping[route.parameters[param].type]) {
+      throw new Error(`The type "${route.parameters[param].type}" is invalid`)
+    }
+  })
+
+  // Make sure fields are present
+  if (!route.fields) {
+    throw new Error(`The route ${route.path} must have a fields value`)
+  }
+  if (Object.keys(route.fields).length === 0 ) {
+    throw new Error(`The route ${route.path} must have a fields value`)
+  }
+
+  // Validate each of the fields
+  Object.keys(route.fields).forEach(field => {
+    if (!route.fields[field].hasOwnProperty('type')) {
+      throw new Error(`The field "${field}" is missing a type`)
+    }
+    if (!typeMapping[route.fields[field].type]) {
+      throw new Error(`The field "${field}" does not have a valid type`)
+    }
+
+    if (!route.fields[field].hasOwnProperty('description') || route.fields[field].description.length === 0) {
+      throw new Error(`The field "${field}" is missing a description`)
+    }
+  })
+
+  // Make sure a handler exists
+  if (!route.handler) {
+    throw new Error(`The route ${route.path} must have a request handler`)
+  }
+
+  // Make sure a handler exists
+  if (route.handler.length != 3) {
+    throw new Error(`The handler of ${route.path} must accept three parameters - req, res, and next`)
+  }
+
+  // Check for the existence of examples
+  if (!route.examples || !route.examples.length) {
+    throw new Error(`The route ${route.path} must have at least one example`)
   }
 }
